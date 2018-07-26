39c4fdecc8181cf53584a3593a2d2190819fc74a
optatum06 <optatum06@naver.com>
2018-07-26 12:17:52 +0900

    OpenGL 붙이기

---
+++ common.h
@@ -0,0 +1,22 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+
+/*=======STANDARD TEMPLATE LIBRARY=======*/
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <memory>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+/*=======GLU/GLEW=======*/
+#include "Dependencies/include/GL/glew.h"
+#include "Dependencies/include/GL/glu.h"
+#include "Dependencies/include/GL/gl.h"
+
+/*=======CLASS DEFINITIONS=======*/
+
+
+#endif
